92bcc2a432e49a314dc05c74461dcc755b2a7536
jason <jason@noisybox.net>
2017-06-20 23:33:05 -0700

    modularize

--- extractilizer.py
+++ extractilizer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import argparse
 import os
-import subprocess
+from ffmpeg_util import *
 
 parser = argparse.ArgumentParser()
 
@@ -24,13 +24,5 @@ filenames = map(lambda x: x.strip(), filenames)
 print("Read %d filenames from %s" % (len(filenames), args.infile))
 for filename in filenames:
     print("Extracting from %s" %(filename))
-    # avconv -i tears_of_steel.mkv -map 0:2 -acodec libmp3lame -ar 44100 -ac 2 -ab 192k tears_of_steel_soundtrack.mp3
-    # https://ffmpeg.org/ffmpeg-filters.html#silenceremove
-    outfile = "%s/out.wav" %(args.outdir)
-    cmd = "ffmpeg -i %s -acodec pcm_s16le -ar 44100 -ac 1 -af silenceremove=0:0:0:-1:0.5:-50dB -y %s" % (filename, outfile)
-    #cmd = "ffmpeg -acodec pcm_s16le -ac 1 -i %s -o %s -y" % (filename, outfile)
-
-    #ffmpeg -i o2/out.wav -af volumedetect -f null /dev/null 2>&1 | grep max_volume | awk '{ print $5 }'
-
-    print(cmd)
-    subprocess.check_call(cmd)
+    extract_audio(filename, "%s/out.wav" % (args.outdir))
+    
